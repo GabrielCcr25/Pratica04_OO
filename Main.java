3d5e8b26b03993298205f45d37719d6ac471944d
Gabriel Lima <108353430+GabrielCcr25@users.noreply.github.com>
2022-10-03 15:31:37 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package br.com.Questao4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Conta cp = new ContaPoupanca();
+		cp.setSaldo(2121);
+		cp.imprimeExtrato();
+
+	}
+
+}
